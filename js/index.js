2c5663525995e27d6fd17e3ef282428581a0eb67
Rajesh01-star <rajeshbehera489@gmail.com>
2022-04-20 17:41:47 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#menu-btn");
+const nav = document.querySelector("#menu");
+btn.addEventListener("click", function (e) {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
